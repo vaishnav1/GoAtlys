47ef5173c47f127fed8548e9d59b682945dbc4dd
vaishnav1 <vaishnav.kukku52@gmail.com>
2024-08-27 19:34:38 +0530

    Added needed view classes

---
+++ Go Atlys/Views/CarouselView.swift
@@ -0,0 +1,15 @@
+//
+//  CarouselView.swift
+//  Go Atlys
+//
+//  Created by Vaishnav on 27/08/24.
+//
+
+import UIKit
+
+class CarouselView: UIView {
+    
+    
+    
+    
+}
---
+++ Go Atlys/Views/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Go Atlys
+//
+//  Created by Vaishnav on 27/08/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+ 
+    //MARK: - Outlets
+    
+    @IBOutlet weak var carouselView: UIView!
+    
+}
